91dfc2d63eb77e1ceaef4ffcb0af7bf679f7eabe
Lars <lars@koelker.dev>
2023-03-26 15:21:32 +0200

    Init

---
+++ manifest.ts
@@ -0,0 +1,44 @@
+import packageJson from "./package.json";
+
+/**
+ * After changing, please reload the extension at `chrome://extensions`
+ */
+const manifest: chrome.runtime.ManifestV3 = {
+  manifest_version: 3,
+  name: packageJson.name,
+  version: packageJson.version,
+  description: packageJson.description,
+  options_page: "src/pages/options/index.html",
+  background: {
+    service_worker: "src/pages/serviceWorker/index.js",
+    type: "module",
+  },
+  action: {
+    default_popup: "src/pages/popup/index.html",
+    default_icon: "64.png",
+  },
+  icons: {
+    "128": "128.png",
+    "64": "64.png",
+    "32": "32.png",
+    "16": "16.png"
+  },
+  permissions: [
+    "storage",
+    "activeTab",
+    "contextMenus"
+  ],
+  default_locale: "en",
+  content_scripts: [
+    {
+      "matches": [ "https://lars.koelker.dev/extensions/note/?version=*" ],
+      "js": [ "src/pages/hideUpdateNotice/index.js" ]
+    }
+  ],
+  web_accessible_resources: [{
+    "resources": [ "manifest.json" ],
+    "matches": [ "https://*.koelker.dev/*" ]
+  }]
+};
+
+export default manifest;
---
+++ src/pages/popup/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "@pages/popup/index.css";
+import Popup from "@pages/popup/Popup";
+import refreshOnUpdate from "virtual:reload-on-update-in-view";
+
+refreshOnUpdate("pages/popup");
+
+function init() {
+  const appContainer = document.querySelector("#root");
+
+  const root = createRoot(appContainer);
+  root.render(<Popup />);
+}
+
+init();
